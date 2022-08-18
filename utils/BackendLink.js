dd7fe3612771268aa17f779c4b0df9e109fd6e1b
shuaib128 <shuaibahamed77@gmail.com>
2022-08-18 13:26:32 -0700

    back chanced env

--- utils/BackendLink.js
+++ utils/BackendLink.js
@@ -1,2 +1,2 @@
-export const BackendLink = "http://localhost:8000"
-// export const BackendLink = "https://squarehackathon.herokuapp.com"
+// export const BackendLink = "http://localhost:8000"
+export const BackendLink = "https://squarehackathon.herokuapp.com"
